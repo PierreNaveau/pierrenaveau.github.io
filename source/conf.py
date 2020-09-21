2ec87687e33cc6c16426d7c3da7be7f4c8150544
Pierre Naveau <pierre.naveau@itsme.be>
2020-09-21 14:30:37 +0200

    msg

--- source/conf.py
+++ source/conf.py
@@ -40,7 +40,8 @@ release = '0.1'
 # ones.
 extensions = [
     'sphinx.ext.githubpages',
-    'sphinxcontrib.contentui'
+    'sphinxcontrib.contentui',
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -77,7 +78,7 @@ pygments_style = None
 # a list of builtin themes.
 #
 html_theme_path = ["./"]
-html_theme = 'basic'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
